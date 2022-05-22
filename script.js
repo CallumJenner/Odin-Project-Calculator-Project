b59c245223461bf8c6968b72cce5525687da54d0
Callum Jenner <callumjenner1@gmail.com>
2022-05-22 13:05:26 +0100

    Start to add JS functionality - still broken

--- script.js
+++ script.js
@@ -26,3 +26,45 @@ function operate(num1, num2, operator) {
             return divide(num1, num2);
     }
 }
+
+const buttons = document.getElementsByClassName("calc-button");
+
+let inputValue;
+let currentTotal = 0;
+let newValue;
+let wasLastInputOperator = true;
+
+for (i of buttons) {
+    let selectedButton;
+    i.addEventListener('click', (e) => {
+        //console.log(e.target.value)
+
+        const isOperator = e.target.classList.contains("calc-operator");
+        const calcOutput = document.getElementById("output-screen");
+
+        if (wasLastInputOperator) {
+            calcOutput.textContent = "";
+        }
+
+        if (isOperator) {
+            if (e.target.value == "calc-equals") {
+                calcOutput.textContent = currentTotal
+            } else {
+                newValue = calcOutput.textContent;
+                inputValue = null;
+                //calcOutput.textContent = e.target.value;
+                wasLastInputOperator = true;
+
+                let currentVal = operate(Number(currentTotal), Number(newValue), e.target.value);
+
+                currentTotal = Number(currentVal);
+                calcOutput.textContent = currentTotal;
+            }
+
+        } else {
+            inputValue = e.target.value;
+            calcOutput.textContent += inputValue;
+            wasLastInputOperator = false;
+        }
+    })
+}
